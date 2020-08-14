5f9df2f3ac174aa88321e5e99484c265391cac00
Valentina Rosenzweig <lumirosenzweig@gmail.com>
2020-08-14 00:47:23 -0300

    Productos arreglados

--- js/products.js
+++ js/products.js
@@ -1,42 +1,37 @@
-var productsArray = [];
-
 function showProductsList(array){
-
-    let htmlContentToAppend = "";
+    let contenido = "";
     for(let i = 0; i < array.length; i++){
-        let products = array[i];
+        let product = array[i];
 
-        htmlContentToAppend += `
+        contenido += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + products.imgSrc + `" alt="` + products.desc + `" class="img-thumbnail">
+                    <img src="` + product.imgSrc + `" alt="` + product.desc + `" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
-                        
-                        <h4 class="mb-1">`+ products.name +`</h4>
-                        <small class="text-muted">` + products.soldCount + ` artículos</small>
+                        <h4 class="mb-1">`+ product.name +`</h4>
+                        <small class="text-muted">` + product.soldCount + ` artículos</small>
                     </div>
-                    <div>` + products.description + `</div>
+                    <div>` + product.description + `</div>
                     <br>
-                    <div class="price"> ` + products.cost + ' ' + products.currency + ` </div>
+                    <div class="price">  `+ product.currency + ' ' + product.cost + ` </div>
                 </div>
             </div>
         </div>
         `
-        document.getElementById("producto").innerHTML = htmlContentToAppend;
     }
+    document.getElementById("producto").innerHTML = contenido;
 }
+
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function(resultObj) {
+        if (resultObj.status === "ok" )
+        {
+        showProductsList(resultObj.data);
+        }
 
+    })
+})
 
-            getJSONData(PRODUCTS_URL).then(function(resultObj) {
-                if (resultObj.status === "ok" )
-                {
-                productsArray = resultObj.data,
-                showProductsList(productsArray);
-            }
-
-            })
-        })
